29c09fe8cf1e37e271b5ddbbda569e0fdca5f54c
IsaiasCarrion <65595034+IsaiasCarrion@users.noreply.github.com>
2021-12-16 15:00:32 -0300

    empezando ToDoList

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+import { Todo, TodoList } from "./classes";
+
+const todoList = new TodoList();
+
+const tarea = new Todo("Aprender Javascript");
+
+todoList.nuevoTodo(tarea);
+
+console.log(todoList);
